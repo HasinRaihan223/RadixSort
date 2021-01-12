21f6f31ec4619169928f6ff99b1e62915925da51
HasinRaihan223 <hraihan20@stuy.edu>
2021-01-12 17:29:38 -0500

    Updated length method

--- Radix.java
+++ Radix.java
@@ -8,30 +8,30 @@ public class Radix {
   }
 
   public static int length(int n){
-    int len = 1;
-    while (n/10 != 0){
-      n /= 10;
-      len++;
+    int length = 0;
+    if (n==0){
+      return 1;
     }
-    return len;
+    else if (n < 0){
+      n = Math.abs(n);
+    }
+    length = (int) (Math.log10(n) + 1);
+    return length;
   }
 
   // public static void merge(MyLinkedList original,MyLinkedList[]buckets){
   //
   // }
 
-  //Tester 
-  // public static void main(String[]args){
-  //   Radix r = new Radix();
-  //   int a =123;
-  //   int b =482;
-  //   int c= 21001245;
-  //   System.out.println(r.nth(a, 0));
-  //   System.out.println(r.nth(b, 2));
-  //   System.out.println(r.nth(c, 0));
-  //   System.out.println(r.length(a));
-  //   System.out.println(r.length(b));
-  //   System.out.println(r.length(c));
-  // }
+  //Tester
+  public static void main(String[]args){
+    Radix r = new Radix();
+    int a =-2345;
+    int b =0;
+    int c= 21001245;
+    System.out.println(r.length(a));
+    System.out.println(r.length(b));
+    System.out.println(r.length(c));
+  }
 
 }
